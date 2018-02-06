a45eeaa7c24eebe8d094af1b447c95292367f4c4
sephirothz87 <sephiroth.zhong@gmail.com>
2018-02-06 23:26:03 +0800

    第一版，已经没用了

--- Tscrapy/spiders/S1Spider.py
+++ Tscrapy/spiders/S1Spider.py
@@ -18,7 +18,10 @@ turn = 1
 match_per_turn = 10
 
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36'
+#如果获取失败，重新去网站刷新一下cookie
 cookie = '__utmc=56961525; LastUrl=; FirstOKURL=http%3A//www.okooo.com/jingcai/; First_Source=www.okooo.com; Hm_lvt_5ffc07c2ca2eda4cc1c4d8e50804c94b=1517120624; Last_Source=http%3A//www.okooo.com/soccer/match/876722/odds/; IMUserName=ok_025742692071; __utmz=56961525.1517821150.16.3.utmcsr=okooo.com|utmccn=(referral)|utmcmd=referral|utmcct=/jingcai/2018-02-03/; __utma=56961525.95899878.1517120624.1517833475.1517848115.18; data_start_isShow=1; PHPSESSID=d932002d848166585fd346e771a90c5131becbce; Hm_lpvt_5ffc07c2ca2eda4cc1c4d8e50804c94b=1517849579; __utmb=56961525.53.8.1517849580034; DRUPAL_LOGGED_IN=Y; IMUserID=24006237; OkAutoUuid=b8b92c071a5ba1185c065aa6d9a2175b; OkMsIndex=5; isInvitePurview=0; UWord=5a9291a00f6e4c9e03f939fab73258b3a2d'
+
+
 
 class S1Spider(scrapy.Spider):
     name = 'S1Spider'
@@ -33,6 +36,7 @@ class S1Spider(scrapy.Spider):
         matches = response.xpath('//*[@id="team_fight_table"]')
         # print(matches.extract())
 
+        # for i in range(2, match_per_turn + 2):
         for i in range(2, match_per_turn + 2):
             # print(i)
             match = matches.xpath('tr[%s]' % (i))
@@ -73,8 +77,7 @@ class S1Spider(scrapy.Spider):
                 'goalAway': goalAway})
 
     def oddDetailparse(self, response):
-
-        print(response)
+        print('oddDetailparse')
 
         williamHillOdds = response.xpath('//*[@id="tr14"]')
 
