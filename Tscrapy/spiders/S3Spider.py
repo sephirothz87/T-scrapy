6d582b953ab72dea35be7dc4b25221a8794a4d89
sephirothz87 <sephiroth.zhong@gmail.com>
2018-02-10 02:51:26 +0800

    整理代码

--- Tscrapy/spiders/S3Spider.py
+++ Tscrapy/spiders/S3Spider.py
@@ -40,10 +40,10 @@ roundWantStart = 1
 # 设置日志级别
 logging.basicConfig(level=logging.DEBUG)
 cookie = su.COOKIE
-# cookie = 'LastUrl=; FirstOKURL=http%3A//www.okooo.com/jingcai/; First_Source=www.okooo.com; Last_Source=http%3A//www.okooo.com/soccer/match/876722/odds/; data_start_isShow=1; OkAutoUuid=b8b92c071a5ba1185c065aa6d9a2175b; OkMsIndex=5; __utmc=56961525; isInvitePurview=0; PHPSESSID=f3438442d0a40ccd389ab57fc3e7620c9536b916; Hm_lvt_5ffc07c2ca2eda4cc1c4d8e50804c94b=1517120624,1517893507; IMUserID=24006237; IMUserName=ok_025742692071; __utmz=56961525.1518109164.32.9.utmcsr=okooo.com|utmccn=(referral)|utmcmd=referral|utmcct=/soccer/league/17/schedule/2746/; DRUPAL_LOGGED_IN=Y; __utma=56961525.95899878.1517120624.1518121003.1518123415.35; UWord=d411d8cd987f00b204e9800998ecf84527e; __utmb=56961525.31.8.1518125819798; Hm_lpvt_5ffc07c2ca2eda4cc1c4d8e50804c94b=1518125837'
 userAgent = su.USER_AGENT
 
 resultData = []
+
 
 def decodeOddLine(oddLine, isHodd):
     print('decodeOddLine')
@@ -194,93 +194,33 @@ class S3Spider(scrapy.Spider):
                 'resultCode': resultCode
             }
 
-            # 获取立博详细赔率
-            # oddLadBrokesDetailUrl = 'http://www.okooo.com/soccer/match/%s/odds/change/%s/' %(matchId,LAD_BROKES)
-            # yield scrapy.Request(url=oddLadBrokesDetailUrl,headers=headers,callback=self.parseOddsDetail,meta=meta)
-
-            # 去比赛详细页面获得赔率
-            # url = 'http://www.okooo.com/soccer/match/%s/odds/' % (matchId)
+            # 获取立博详细赔率 ajax
             url = 'http://www.okooo.com/soccer/match/%s/odds/ajax/?page=0&trnum=0&companytype=BaijiaBooks&type=1' % (
-            matchId)
-            # url = 'http://www.okooo.com/soccer/match/135290/odds/ajax/?page=0&trnum=0&companytype=BaijiaBooks&type=1'
-            # 'http://www.okooo.com/soccer/match/877199/odds/ajax/?page=0&trnum=0&companytype=BaijiaBooks&type=1'
-
-            # cookie = {
-            #     '__utma':'56961525.95899878.1517120624.1518169593.1518186546.38',
-            #     '__utmb':'56961525.5.9.1518186550970',
-            #     '__utmc':'56961525',
-            #     '__utmz':{'56961525.1518109164.32.9.utmcsr':'okooo.com|utmccn=(referral)|utmcmd=referral|utmcct=/soccer/league/17/schedule/2746/'},
-            #     'data_start_isShow':'1',
-            #     'DRUPAL_LOGGED_IN':'Y',
-            #     'First_Source':'www.okooo.com',
-            #     'FirstOKURL':'http%3A//www.okooo.com/jingcai/',
-            #     'Hm_lpvt_5ffc07c2ca2eda4cc1c4d8e50804c94b':'1518186551',
-            #     'Hm_lvt_5ffc07c2ca2eda4cc1c4d8e50804c94b':'1517120624,1517893507',
-            #     'IMUserID':'24006237',
-            #     'IMUserName':'ok_025742692071',
-            #     'isInvitePurview':'0',
-            #     'Last_Source':'http%3A//www.okooo.com/soccer/match/876722/odds/',
-            #     'LastUrl':'',
-            #     'OkAutoUuid':'b8b92c071a5ba1185c065aa6d9a2175b',
-            #     'OkMsIndex':'5',
-            #     'PHPSESSID':'f3438442d0a40ccd389ab57fc3e7620c9536b916',
-            #     'UWord':'d441d8cd987f00b204e9800998ecf84927e'
-            # }
+                matchId)
             headers = {
                 'User-Agent': userAgent,
                 'Connection': 'keep-alive',
                 'Referer': 'http://www.okooo.com/soccer/match/%s/odds/' % (matchId),
-                # 'Cookie': cookie,
-                # 'Cookie':'LastUrl=; FirstOKURL=http%3A//www.okooo.com/jingcai/; First_Source=www.okooo.com; Last_Source=http%3A//www.okooo.com/soccer/match/876722/odds/; data_start_isShow=1; OkAutoUuid=b8b92c071a5ba1185c065aa6d9a2175b; OkMsIndex=5; __utmc=56961525; isInvitePurview=0; PHPSESSID=f3438442d0a40ccd389ab57fc3e7620c9536b916; Hm_lvt_5ffc07c2ca2eda4cc1c4d8e50804c94b=1517120624,1517893507; IMUserID=24006237; IMUserName=ok_025742692071; __utmz=56961525.1518109164.32.9.utmcsr=okooo.com|utmccn=(referral)|utmcmd=referral|utmcct=/soccer/league/17/schedule/2746/; DRUPAL_LOGGED_IN=Y; __utma=56961525.95899878.1517120624.1518121003.1518123415.35; UWord=d411d8cd987f00b204e9800998ecf84527e; __utmb=56961525.37.8.1518126519745; Hm_lpvt_5ffc07c2ca2eda4cc1c4d8e50804c94b=1518126698',
                 'X-Requested-With': 'XMLHttpRequest',
                 'Accept-Language': 'zh-CN,zh;q=0.9,ja;q=0.8,en;q=0.7,zh-TW;q=0.6',
                 'Accept': 'text/html, */*',
                 'Pragma': 'no-cache',
                 'Proxy-Connection': 'keep-alive'
             }
-            # 'Origin:http://www.okooo.com'
             yield scrapy.Request(url=url, headers=headers, cookies=cookie, callback=self.parseOddsDetail, meta=meta)
 
     def parseOddsDetail(self, response):
         d('parseOddsDetail')
-        # d(type(response))
-        # d(response.text)
-        # d(response.meta)
-
-        # d('response.body')
-        # d(response.body)
 
         meta = response.meta
-        # i('Round: %s Match: %s %s' % (meta['match']['round'], meta['match']['matchId'], meta['match']['vsText']))
 
-        # # startTime = response.xpath('body/div[4]/div[4]/div[1]/div/div[2]/p[1]/text()')
-        # startTime = response.xpath('//div[@class="qbx_2"]')
-        # # startTime = response.xpath('//div[@class="qbx_2"]/p[1]/text()')
-        # d(startTime)
-
-        # oddLine = response.xpath('//*[@id="tr82"]')
-        # oddLine = response.xpath('/html/body/span[38]')
-        # oddLine = response.xpath('*[@id="tr82"]')
-
-        # sel = Selector(response)
         sel = Selector(response)
         oddLine = sel.xpath('//tr[@id="tr82"]')
-        # oddLine = sel.xpath('/html/body/span[38]')
-
-        # d('oddLine'+oddLine.extract_first())
-        # d(oddLine.extract())
 
         if len(oddLine.extract()) > 0:
-            # d('oddLine nnnnoooooooooooooooot None')
-            # d('oddLine : ')
-            # print('=================================',oddLine.extract_first(),'=================================',)
-            # '/html/body/span[38]'
             odd3 = oddLine.xpath('td[6]/a/span/text()').extract_first()
             odd1 = oddLine.xpath('td[7]/a/span/text()').extract_first()
             odd0 = oddLine.xpath('td[8]/div/a/span/text()').extract_first()
-
-            # d('odd3odd3odd3odd3odd3odd3odd3odd3odd3 : ')
-            # d(odd3)
 
             oddStart3 = oddLine.xpath('td[3]/span/text()').extract_first()
             oddStart1 = oddLine.xpath('td[4]/span/text()').extract_first()
@@ -312,7 +252,6 @@ class S3Spider(scrapy.Spider):
                 hResultCode = 1
 
             meta['match'].update({
-                # 'startTime': startTime,
                 'hoddNum': hoddNum,
                 'hResultCode': hResultCode,
                 'odd3': odd3,
@@ -330,22 +269,16 @@ class S3Spider(scrapy.Spider):
                 'oddStart0': oddStart0,
             })
 
-            # d(meta)
-
             headers = {
                 'User-Agent': userAgent,
                 'Connection': 'keep-alive',
                 'Referer': 'http://www.okooo.com/soccer/match/%s/' % (meta['match']['matchId']),
                 'X-Requested-With': 'XMLHttpRequest',
-                # 'Cookie': su.COOKIE
             }
 
-            # 去比赛让球详细页面获得赔率
-            # url = 'http://www.okooo.com/soccer/match/%s/hodds/' % (meta['match']['matchId'])
-            # yield scrapy.Request(url=url, headers=headers, callback=self.parseHoddsDetail, meta=meta)
+            # 获取让球详细赔率 ajax
             url = 'http://www.okooo.com/soccer/match/%s/hodds/ajax/?page=0&trnum=0&companytype=BaijiaBooks' % (
-            meta['match']['matchId'])
-            # yield scrapy.Request(url=url, headers=headers, callback=self.parseHoddsDetail, meta=meta)
+                meta['match']['matchId'])
             yield scrapy.Request(url=url, headers=headers, cookies=cookie, callback=self.parseHoddsDetail, meta=meta)
         else:
             w('no LabBroke odd')
@@ -358,8 +291,6 @@ class S3Spider(scrapy.Spider):
         hodd = str(meta['match']['hoddNum'])
 
         oddLine = response.xpath('//tr[contains(@id,"%s") and td[3]/span/text()="%s"]' % (labBrokes, hodd))
-        # oddLine = response.xpath('//tr[contains(@id,"%s")]' % (labBrokes))#这个可以用
-
 
         print(oddLine)
         print(oddLine.extract())
@@ -383,18 +314,18 @@ class S3Spider(scrapy.Spider):
             hoddStart0 = oddLine.xpath('td[6]/a/span/text()').extract_first()
 
             meta['match'].update({
-                'hodd3':hodd3,
-                'hodd1':hodd1,
-                'hodd0':hodd0,
-                'hoddStart3':hoddStart3,
-                'hoddStart1':hoddStart1,
-                'hoddStart0':hoddStart0,
-                'hoddFrom':hoddFrom
+                'hodd3': hodd3,
+                'hodd1': hodd1,
+                'hodd0': hodd0,
+                'hoddStart3': hoddStart3,
+                'hoddStart1': hoddStart1,
+                'hoddStart0': hoddStart0,
+                'hoddFrom': hoddFrom
             })
         else:
             w('no hodd found')
 
-        #最后再去取一下精确时间
+        # 最后再去取一下精确时间
         match_url = 'http://www.okooo.com/soccer/match/%s/odds/' % (meta['match']['matchId'])
         round_url = 'http://www.okooo.com/soccer/league/%s/schedule/%s/1-1-%s/' % (leagueCode, seasonCode, i)
 
@@ -412,20 +343,8 @@ class S3Spider(scrapy.Spider):
 
         meta = response.meta
 
-        # startTime = response.xpath('body/div[4]/div[4]/div[1]/div/div[2]/p[1]/text()').extract_first()
-        # startTime = response.xpath('/html/body/div[4]/div[4]/div[1]/div/div[2]/p[1]')
-        # startTime = response.xpath('//div[@class="qbx_2"]')
         startTime = response.xpath('//div[@class="qbx_2"]/p[1]/text()').extract_first()
-        startTime = " ".join(startTime.split())#截去中间夹的\xa0 空格符&npsp
-        print(startTime)
-        # startTime.replace('\\xa0','')
-
-        # sel = Selector(response)
-        # startTime = sel.xpath('body')
-        # startTime = sel.xpath('body/div[4]/div[4]/div[1]/div/div[2]/p[1]/text()')
-        # startTime = sel.xpath('body/div[@class="qbx_2"]')
-
-        d(startTime)
+        startTime = " ".join(startTime.split())  # 截去中间夹的\xa0 空格符&npsp
 
         meta['match'].update({
             'startTime': startTime
@@ -433,6 +352,7 @@ class S3Spider(scrapy.Spider):
 
         print(meta['match'])
         resultData.append(meta['match'])
+
 
 process = CrawlerProcess({
     'USER_AGENT': su.USER_AGENT
@@ -459,6 +379,7 @@ def writeCsv(fileName, matchObj):
     f.write(line)
     f.write('\n')
     f.close()
+
 
 for matchData in resultData:
     writeCsv(resultCsvFile, matchData)
